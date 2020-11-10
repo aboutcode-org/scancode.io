4e61649950492231c55d32d4b44c67eb8307b343
Thomas Druez <tdruez@nexb.com>
2020-11-10 18:46:44 +0400

    Make column values filters link for admin changelist #36
    Signed-off-by: Thomas Druez <tdruez@nexb.com>

--- scanpipe/admin.py
+++ scanpipe/admin.py
@@ -21,10 +21,23 @@
 # Visit https://github.com/nexB/scancode.io for support and download.
 
 from django.contrib import admin
+from django.contrib.admin.views.main import ChangeList
+from django.http import QueryDict
+from django.utils.html import format_html
 
 from scanpipe.models import CodebaseResource
 from scanpipe.models import DiscoveredPackage
 from scanpipe.models import ProjectError
+
+
+class InjectRequestChangeList(ChangeList):
+    def get_results(self, request):
+        """
+        Inject the `request` on each object of the results_list.
+        """
+        super().get_results(request)
+        for obj in self.result_list:
+            obj._request = request
 
 
 class ProjectRelatedModelAdmin(admin.ModelAdmin):
@@ -39,24 +52,75 @@ class ProjectRelatedModelAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
+    def get_changelist(self, request, **kwargs):
+        return InjectRequestChangeList
+
+    def project_filter(self, obj):
+        return format_html(
+            '<a href="?project__uuid__exact={project_uuid}">{project}</a>',
+            project=obj.project,
+            project_uuid=obj.project.uuid,
+        )
+
+    project_filter.short_description = "Project"
+    project_filter.admin_order_field = "project"
+
+    @staticmethod
+    def as_filter(obj, field_name):
+        field_value = getattr(obj, field_name, None)
+        if field_value is None:
+            return
+
+        request = getattr(obj, "_request")
+        if request:
+            query_dict = request.GET.copy()
+        else:
+            query_dict = QueryDict()
+        query_dict[field_name] = field_value
+
+        return format_html(
+            '<a href="?{query}">{field_value}</a>',
+            query=query_dict.urlencode(),
+            field_value=field_value,
+        )
+
+    def type_filter(self, obj):
+        return self.as_filter(obj, field_name="type")
+
+    type_filter.short_description = "Type"
+    type_filter.admin_order_field = "type"
+
+    def status_filter(self, obj):
+        return self.as_filter(obj, field_name="status")
+
+    status_filter.short_description = "Status"
+    status_filter.admin_order_field = "status"
+
+    def programming_language_filter(self, obj):
+        return self.as_filter(obj, field_name="programming_language")
+
+    programming_language_filter.short_description = "Programming language"
+    programming_language_filter.admin_order_field = "programming_language"
+
 
 @admin.register(CodebaseResource)
 class CodebaseResourceAdmin(ProjectRelatedModelAdmin):
     list_display = (
-        "project",
+        "project_filter",
         "path",
-        "status",
-        "type",
+        "status_filter",
+        "type_filter",
         "size",
         "name",
         "extension",
-        "programming_language",
+        "programming_language_filter",
         "mime_type",
         "file_type",
         "license_expressions",
         "copyrights",
         "for_packages",
     )
+    list_display_links = ("path",)
     list_filter = ("project", "status", "type", "programming_language")
     search_fields = ("path", "mime_type", "file_type")
 
@@ -70,15 +134,16 @@ class CodebaseResourceInline(admin.TabularInline):
 @admin.register(DiscoveredPackage)
 class DiscoveredPackageAdmin(ProjectRelatedModelAdmin):
     list_display = (
-        "project",
+        "project_filter",
         "package_url",
-        "type",
+        "type_filter",
         "namespace",
         "name",
         "version",
         "license_expression",
         "copyright",
     )
+    list_display_links = ("package_url",)
     list_filter = ("project", "type")
     search_fields = ("name", "namespace", "description", "codebase_resources__path")
     exclude = ("codebase_resources",)
@@ -87,6 +152,7 @@ class DiscoveredPackageAdmin(ProjectRelatedModelAdmin):
 
 @admin.register(ProjectError)
 class ProjectErrorAdmin(ProjectRelatedModelAdmin):
-    list_display = ("project", "model", "message", "created_date", "uuid")
+    list_display = ("project_filter", "model", "message", "created_date", "uuid")
+    list_display_links = ("message",)
     list_filter = ("project", "model")
     search_fields = ("uuid", "message")
