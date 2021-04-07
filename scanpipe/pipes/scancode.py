80d310e1a25e4e16fb09aedc367491acc408dcc9
Thomas Druez <tdruez@nexb.com>
2021-04-07 18:05:28 +0200

    Refactor scan_file to reuse scancode-toolkit scan_resource and benefit from timeout support #135
    Signed-off-by: Thomas Druez <tdruez@nexb.com>

--- scanpipe/pipes/scancode.py
+++ scanpipe/pipes/scancode.py
@@ -35,7 +35,9 @@ from commoncode.resource import VirtualCodebase
 from extractcode.extract import extract_file
 from packageurl import PackageURL
 from scancode import ScancodeError
+from scancode import Scanner
 from scancode import api as scancode_api
+from scancode import cli as scancode_cli
 
 from scanpipe import pipes
 from scanpipe.models import CodebaseResource
@@ -122,26 +124,23 @@ def get_resource_info(location):
 
 def scan_file(location):
     """
-    Run a license, copyright, email, and url scan functions on provided `location`.
-    Return a dict of `scan_results` and a list of `scan_errors`.
+    Run a license, copyright, email, and url scan functions on provided `location`,
+    using the scancode-toolkit scan_resource method to support timeout.
+
+    Return a dict of scan `results` and a list of `errors`.
     """
-    scan_functions = [
-        scancode_api.get_copyrights,
-        partial(scancode_api.get_licenses, include_text=True),
-        scancode_api.get_emails,
-        scancode_api.get_urls,
+    scanners = [
+        Scanner("copyrights", scancode_api.get_copyrights),
+        Scanner("licenses", partial(scancode_api.get_licenses, include_text=True)),
+        Scanner("emails", scancode_api.get_emails),
+        Scanner("urls", scancode_api.get_urls),
     ]
 
-    scan_results = {}
-    scan_errors = []
+    # `rid` is not needed in this context, yet required in the scan_resource args
+    location_rid = location, 0
+    _, _, errors, _, results, _ = scancode_cli.scan_resource(location_rid, scanners)
 
-    for function in scan_functions:
-        try:
-            scan_results.update(function(location))
-        except Exception as scan_error:
-            scan_errors.append(scan_error)
-
-    return scan_results, scan_errors
+    return results, errors
 
 
 def scan_file_and_save_results(codebase_resource):
