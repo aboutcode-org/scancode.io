de13803877b5da2450156a279e13b1cc2f88543a
AvishrantSharma <avishrants@gmail.com>
2021-04-12 20:32:56 +0530

    Add option to drag and drop files in the "Inputs" section of the forms  #109
    Signed-off-by: Avishrant Sharma <avishrants@gmail.com>

--- scancodeio/static/add-inputs.js
+++ scancodeio/static/add-inputs.js
@@ -21,18 +21,39 @@
 // Visit https://github.com/nexB/scancode.io for support and download.
 
 const fileInput = document.querySelector('#id_input_files');
-fileInput.onchange = () => {
-  if (fileInput.files.length > 0) {
-    const fileName = document.querySelector('#inputs-file-name');
-    fileName.innerHTML = "";
-    for (file of fileInput.files) {
-      fileName.innerHTML += `<span class="is-block">${file.name}</span>`
-    }
-  }
-}
+fileInput.onchange = update_files;
 
 const input_urls = document.querySelector('#id_input_urls');
 input_urls.oninput = () => {
   input_urls.style.height = "";
   input_urls.style.height = input_urls.scrollHeight + 3 + "px";
+}
+
+// Handling DropZone Events
+const dropzone = document.querySelector('#file_upload_label');
+dropzone.addEventListener("dragenter", disable_event);
+dropzone.addEventListener("dragover", disable_event);
+dropzone.addEventListener("drop", dropHandler);
+
+function disable_event(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+function dropHandler(event) {
+  // Prevent default behavior (Prevent file from being opened)
+  disable_event(event);
+  document.querySelector('#id_input_files').files = event.dataTransfer.files;
+  update_files();
+}
+
+// Function for updating file_names in span
+function update_files() {
+  if (fileInput.files.length > 0) {
+    const fileName = document.querySelector('#inputs-file-name');
+    fileName.innerHTML = "";
+    for (file of fileInput.files) {
+      fileName.innerHTML += `<span class="is-block">${file.name}</span>`;
+    }
+  }
 }
