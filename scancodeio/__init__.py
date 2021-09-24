9424dff5a6c2f38fbe6993eb4dba6d764441c46c
Thomas Druez <tdruez@nexb.com>
2021-09-24 16:34:16 +0400

    Turn off the warnings from the `parameter_expansion` module
    Signed-off-by: Thomas Druez <tdruez@nexb.com>

--- scancodeio/__init__.py
+++ scancodeio/__init__.py
@@ -22,6 +22,7 @@
 
 import os
 import sys
+import warnings
 from pathlib import Path
 
 from django.conf import settings
@@ -33,6 +34,10 @@ from scancodeio.celery import app as celery_app
 __version__ = "21.9.6"
 
 SCAN_NOTICE = Path(__file__).resolve().parent.joinpath("scan.NOTICE").read_text()
+
+
+# Turn off the warnings from the `parameter_expansion` module.
+warnings.filterwarnings("ignore", module="parameter_expansion")
 
 
 # Resolve and set the workspace location from the settings.
