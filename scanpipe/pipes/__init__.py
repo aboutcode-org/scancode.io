e4c9f537809f07e56b513901bb5d4149e0745a4a
Thomas Druez <tdruez@nexb.com>
2021-12-03 11:38:31 +0400

    Debug test failure #367
    Signed-off-by: Thomas Druez <tdruez@nexb.com>

--- scanpipe/pipes/__init__.py
+++ scanpipe/pipes/__init__.py
@@ -211,9 +211,11 @@ def run_command(cmd, log_output=False):
         cmd = " ".join(cmd)
 
     if not log_output:
+        print("not log_output")
         exitcode, output = subprocess.getstatusoutput(cmd)
         return exitcode, output
 
+    print("before subprocess.Popen")
     process = subprocess.Popen(
         cmd,
         shell=True,
@@ -221,6 +223,7 @@ def run_command(cmd, log_output=False):
         stderr=subprocess.STDOUT,
         universal_newlines=True,
     )
+    print("after subprocess.Popen")
 
     while _stream_process(process):
         sleep(1)
