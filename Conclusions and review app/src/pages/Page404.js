f19c3f654784b910a03f8f98ce7594f313be228d
Viraj Dhanushka <59219626+viradhanus@users.noreply.github.com>
2022-04-20 02:43:11 +0530

    Rename the title
    Signed-off-by: viradhanus <e16083@eng.pdn.ac.lk>

--- Conclusions and review app/src/pages/Page404.js
+++ Conclusions and review app/src/pages/Page404.js
@@ -21,7 +21,7 @@ const RootStyle = styled(Page)(({ theme }) => ({
 
 export default function Page404() {
   return (
-    <RootStyle title="404 Page Not Found | OWASP">
+    <RootStyle title="404 Page Not Found | ScanCode">
       <Container>
         <MotionContainer initial="initial" open>
           <Box sx={{ maxWidth: 480, margin: 'auto', textAlign: 'center' }}>
