5bf23bdd67a745700759c4d63d645b7931cf7748
Jono Yang <JonoYang@users.noreply.github.com>
2022-06-17 00:39:23 -0700

    448 none package uid (#454)
    * Scope the packages QuerySet by current project #448
    
    Signed-off-by: Thomas Druez <tdruez@nexb.com>
    
    * Bump scancode to 31.0.0rc2 #448
    
        * The new scancode version provides a fix to the error encountered in #448
        * Update expected test results
    
    Signed-off-by: Jono Yang <jyang@nexb.com>
    
    * Ensure package_uid unique and indexed #448
    
        * TODO: Do we want to make blank and null true on the field since we have existing empty values and the pipelines do not set a package_uid when creating a DiscoveredPackage?
    
    Signed-off-by: Jono Yang <jyang@nexb.com>
    
    * Make DiscoveredPackage.package_uid unique within a Project #448
    
    Signed-off-by: Thomas Druez <tdruez@nexb.com>
    
    * Simplify update_or_create_package and fix unit test #448
    
    Add a override option to update_from_data
    
    Signed-off-by: Thomas Druez <tdruez@nexb.com>
    
    Co-authored-by: Thomas Druez <tdruez@nexb.com>

--- scanpipe/models.py
+++ scanpipe/models.py
@@ -1729,6 +1729,7 @@ class DiscoveredPackage(
     package_uid = models.CharField(
         max_length=1024,
         blank=True,
+        db_index=True,
         help_text=_("Unique identifier for this package."),
     )
 
@@ -1740,6 +1741,13 @@ class DiscoveredPackage(
 
     class Meta:
         ordering = ["uuid"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["project", "package_uid"],
+                condition=~Q(package_uid=""),
+                name="%(app_label)s_%(class)s_unique_package_uid_within_project",
+            ),
+        ]
 
     def __str__(self):
         return self.package_url or str(self.uuid)
@@ -1807,7 +1815,7 @@ class DiscoveredPackage(
         discovered_package.save(save_error=False, capture_exception=False)
         return discovered_package
 
-    def update_from_data(self, package_data):
+    def update_from_data(self, package_data, override=False):
         """
         Update this discovered package instance with the provided `package_data`.
         The `save()` is called only if at least one field was modified.
@@ -1825,11 +1833,9 @@ class DiscoveredPackage(
                 continue
 
             current_value = getattr(self, field_name, None)
-            if not current_value:
+            if not current_value or (current_value != value and override):
                 setattr(self, field_name, value)
                 updated_fields.append(field_name)
-            elif current_value != value:
-                pass  # TODO: handle this case
 
         if updated_fields:
             self.save()
