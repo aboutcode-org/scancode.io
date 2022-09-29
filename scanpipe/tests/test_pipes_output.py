bf3f78067386614ad7458278c573853f70da0f2d
tdruez <489057+tdruez@users.noreply.github.com>
2022-09-29 17:55:30 +0400

    Refactor the output.py module for better queries performances #532 (#533)
    Signed-off-by: Thomas Druez <tdruez@nexb.com>

--- scanpipe/tests/test_pipes_output.py
+++ scanpipe/tests/test_pipes_output.py
@@ -20,20 +20,176 @@
 # ScanCode.io is a free software code scanning tool from nexB Inc. and others.
 # Visit https://github.com/nexB/scancode.io for support and download.
 
+import collections
+import json
 import shutil
 import tempfile
 from pathlib import Path
-from unittest import TestCase
+from unittest import mock
+
+from django.core.management import call_command
+from django.test import TestCase
 
 import xlsxwriter
 from lxml import etree
 
+from scanpipe.models import CodebaseResource
+from scanpipe.models import Project
+from scanpipe.models import ProjectError
 from scanpipe.pipes import output
+from scanpipe.tests import mocked_now
+from scanpipe.tests import package_data1
 
 
-class ScanPipeOutputTest(TestCase):
+class ScanPipeOutputPipesTest(TestCase):
     data_location = Path(__file__).parent / "data"
 
+    def test_scanpipe_pipes_outputs_queryset_to_csv_file(self):
+        project1 = Project.objects.create(name="Analysis")
+        codebase_resource = CodebaseResource.objects.create(
+            project=project1,
+            path="filename.ext",
+        )
+        codebase_resource.create_and_add_package(package_data1)
+
+        queryset = project1.discoveredpackages.all()
+        fieldnames = ["purl", "name", "version"]
+
+        output_file_path = project1.get_output_file_path("packages", "csv")
+        with output_file_path.open("w") as output_file:
+            output.queryset_to_csv_file(queryset, fieldnames, output_file)
+
+        expected = [
+            "purl,name,version\n",
+            "pkg:deb/debian/adduser@3.118?arch=all,adduser,3.118\n",
+        ]
+        with output_file_path.open() as f:
+            self.assertEqual(expected, f.readlines())
+
+        queryset = project1.codebaseresources.all()
+        fieldnames = ["for_packages", "path"]
+        output_file_path = project1.get_output_file_path("resources", "csv")
+        with output_file_path.open("w") as output_file:
+            output.queryset_to_csv_file(queryset, fieldnames, output_file)
+
+        package_uid = "pkg:deb/debian/adduser@3.118?uuid=610bed29-ce39-40e7-92d6-fd8b"
+        expected = [
+            "for_packages,path\n",
+            f"['{package_uid}'],filename.ext\n",
+        ]
+        with output_file_path.open() as f:
+            self.assertEqual(expected, f.readlines())
+
+    def test_scanpipe_pipes_outputs_queryset_to_csv_stream(self):
+        project1 = Project.objects.create(name="Analysis")
+        codebase_resource = CodebaseResource.objects.create(
+            project=project1,
+            path="filename.ext",
+        )
+        codebase_resource.create_and_add_package(package_data1)
+
+        queryset = project1.discoveredpackages.all()
+        fieldnames = ["purl", "name", "version"]
+
+        output_file = project1.get_output_file_path("packages", "csv")
+        with output_file.open("w") as output_stream:
+            generator = output.queryset_to_csv_stream(
+                queryset, fieldnames, output_stream
+            )
+            collections.deque(generator, maxlen=0)  # Exhaust the generator
+
+        expected = [
+            "purl,name,version\n",
+            "pkg:deb/debian/adduser@3.118?arch=all,adduser,3.118\n",
+        ]
+        with output_file.open() as f:
+            self.assertEqual(expected, f.readlines())
+
+        queryset = project1.codebaseresources.all()
+        fieldnames = ["for_packages", "path"]
+        output_file = project1.get_output_file_path("resources", "csv")
+        with output_file.open("w") as output_stream:
+            generator = output.queryset_to_csv_stream(
+                queryset, fieldnames, output_stream
+            )
+            collections.deque(generator, maxlen=0)  # Exhaust the generator
+
+        output.queryset_to_csv_stream(queryset, fieldnames, output_file)
+        package_uid = "pkg:deb/debian/adduser@3.118?uuid=610bed29-ce39-40e7-92d6-fd8b"
+        expected = [
+            "for_packages,path\n",
+            f"['{package_uid}'],filename.ext\n",
+        ]
+        with output_file.open() as f:
+            self.assertEqual(expected, f.readlines())
+
+    @mock.patch("scanpipe.pipes.datetime", mocked_now)
+    def test_scanpipe_pipes_outputs_to_csv(self):
+        fixtures = self.data_location / "asgiref-3.3.0_fixtures.json"
+        call_command("loaddata", fixtures, **{"verbosity": 0})
+        project = Project.objects.get(name="asgiref")
+
+        with self.assertNumQueries(7):
+            output_files = output.to_csv(project=project)
+
+        csv_files = [
+            "codebaseresource-2010-10-10-10-10-10.csv",
+            "discovereddependency-2010-10-10-10-10-10.csv",
+            "discoveredpackage-2010-10-10-10-10-10.csv",
+            "projecterror-2010-10-10-10-10-10.csv",
+        ]
+
+        for csv_file in csv_files:
+            self.assertIn(csv_file, project.output_root)
+            self.assertIn(csv_file, [f.name for f in output_files])
+
+    def test_scanpipe_pipes_outputs_to_json(self):
+        fixtures = self.data_location / "asgiref-3.3.0_fixtures.json"
+        call_command("loaddata", fixtures, **{"verbosity": 0})
+        project = Project.objects.get(name="asgiref")
+
+        output_file = output.to_json(project=project)
+        self.assertIn(output_file.name, project.output_root)
+
+        with output_file.open() as f:
+            results = json.loads(f.read())
+
+        expected = ["dependencies", "files", "headers", "packages"]
+        self.assertEqual(expected, sorted(results.keys()))
+
+        self.assertEqual(1, len(results["headers"]))
+        self.assertEqual(18, len(results["files"]))
+        self.assertEqual(2, len(results["packages"]))
+        self.assertEqual(4, len(results["dependencies"]))
+
+        self.assertIn("compliance_alert", results["files"][0])
+
+        # Make sure the output can be generated even if the work_directory was wiped
+        shutil.rmtree(project.work_directory)
+        with self.assertNumQueries(7):
+            output_file = output.to_json(project=project)
+        self.assertEqual([output_file.name], project.output_root)
+
+    def test_scanpipe_pipes_outputs_to_xlsx(self):
+        fixtures = self.data_location / "asgiref-3.3.0_fixtures.json"
+        call_command("loaddata", fixtures, **{"verbosity": 0})
+
+        project = Project.objects.get(name="asgiref")
+        ProjectError.objects.create(
+            project=project, model="Model", details={}, message="Error"
+        )
+
+        output_file = output.to_xlsx(project=project)
+        self.assertIn(output_file.name, project.output_root)
+
+        # Make sure the output can be generated even if the work_directory was wiped
+        shutil.rmtree(project.work_directory)
+        with self.assertNumQueries(7):
+            output_file = output.to_xlsx(project=project)
+        self.assertEqual([output_file.name], project.output_root)
+
+
+class ScanPipeXLSXOutputPipesTest(TestCase):
     def test__add_xlsx_worksheet_does_truncates_long_strings_over_max_len(self):
         # This test verifies that we do not truncate long text silently
 
