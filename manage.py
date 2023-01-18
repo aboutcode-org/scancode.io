385087f2b7d9fa4e9dae4b1c38da442d629d31bc
Jono Yang <jyang@nexb.com>
2023-01-17 17:22:25 -0800

    Dedupe leading argv entries in manage.py
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- manage.py
+++ manage.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
 if __name__ == "__main__":
+    import sys
+    if len(sys.argv) >= 2:
+        if sys.argv[0] == sys.argv[1]:
+            sys.argv.pop(0)
     from scancodeio import command_line
 
     command_line()
