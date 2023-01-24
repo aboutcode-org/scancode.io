86b1b9c9a8d6141765db0977443e62fa39c49a4a
Jono Yang <jyang@nexb.com>
2023-01-24 10:27:32 -0800

    Remove argv dedup code from manage.py
        - Print argv list for debug purposes
    
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- manage.py
+++ manage.py
@@ -2,9 +2,8 @@
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) >= 2:
-        if sys.argv[0] == sys.argv[1]:
-            sys.argv.pop(0)
+    print(sys.argv)
+  
     from scancodeio import command_line
 
     command_line()
