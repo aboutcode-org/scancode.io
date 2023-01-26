ff510b067a9b0c0e639be60c8c9fea651241fbaa
Jono Yang <jyang@nexb.com>
2023-01-25 17:42:50 -0800

    Reintroduce manage.py argv dedup
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- manage.py
+++ manage.py
@@ -3,7 +3,11 @@
 if __name__ == "__main__":
     import sys
     print(sys.argv)
-  
+
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        if arg.endswith("manage.py"):
+            sys.argv.pop(i)
+
     from scancodeio import command_line
 
     command_line()
