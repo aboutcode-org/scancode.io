1449ff065337cde58e6c8226203f931bfe422fd2
Thomas Druez <tdruez@nexb.com>
2023-04-05 15:44:51 +0400

    Ignore warnings in manage.py file
    Signed-off-by: Thomas Druez <tdruez@nexb.com>

--- manage.py
+++ manage.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+import warnings
+
+# Turn off the warnings for the following modules.
+# This filtering needs to be applied before the modules are imported.
+warnings.filterwarnings("ignore", module="extractcode")
+warnings.filterwarnings("ignore", module="typecode")
+
+
 if __name__ == "__main__":
     from scancodeio import command_line
 
