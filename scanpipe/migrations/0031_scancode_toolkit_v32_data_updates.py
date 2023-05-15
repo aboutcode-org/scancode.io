29ff6a15fbe956c8eb886fd2b883db8b9054e2b8
Thomas Druez <tdruez@nexb.com>
2023-05-15 10:53:47 +0400

    Optimize data migration using bulk_update #569
    Signed-off-by: Thomas Druez <tdruez@nexb.com>

--- scanpipe/migrations/0031_scancode_toolkit_v32_data_updates.py
+++ scanpipe/migrations/0031_scancode_toolkit_v32_data_updates.py
@@ -12,20 +12,31 @@ def compute_package_declared_license_expression_spdx(apps, schema_editor):
     from licensedcode.cache import build_spdx_license_expression
 
     DiscoveredPackage = apps.get_model("scanpipe", "DiscoveredPackage")
-    packages = DiscoveredPackage.objects.filter(
+    queryset = DiscoveredPackage.objects.filter(
         ~Q(declared_license_expression="") & Q(declared_license_expression_spdx="")
-    )
+    ).only("declared_license_expression")
 
-    package_count = packages.count()
-    print(f"Compute declared_license_expression_spdx for {package_count:,} packages.")
+    object_count = queryset.count()
+    print(f"\nCompute declared_license_expression_spdx for {object_count:,} packages.")
 
     chunk_size = 2000
-    for index, package in enumerate(packages.iterator(chunk_size=chunk_size)):
-        if not (index % chunk_size):
-            print(f"  {index:,} / {package_count:,}")
+    iterator = queryset.iterator(chunk_size=chunk_size)
+
+    unsaved_objects = []
+    for index, package in enumerate(iterator, start=1):
         if spdx := build_spdx_license_expression(package.declared_license_expression):
             package.declared_license_expression_spdx = spdx
-            package.save()
+            unsaved_objects.append(package)
+
+        if not (index % chunk_size) and unsaved_objects:
+            print(f"  {index:,} / {object_count:,} computed")
+
+    print("Updating DB objects...")
+    DiscoveredPackage.objects.bulk_update(
+        objs=unsaved_objects,
+        fields=["declared_license_expression_spdx"],
+        batch_size=1000,
+    )
 
 
 def compute_resource_detected_license_expression(apps, schema_editor):
@@ -37,21 +48,44 @@ def compute_resource_detected_license_expression(apps, schema_editor):
     from licensedcode.cache import build_spdx_license_expression
 
     CodebaseResource = apps.get_model("scanpipe", "CodebaseResource")
-    resources = CodebaseResource.objects.filter(~Q(license_expressions=[]))
+    queryset = CodebaseResource.objects.filter(~Q(license_expressions=[])).only(
+        "license_expressions"
+    )
 
-    resource_count = resources.count()
-    print(f"Compute detected_license_expression for {resource_count:,} resources.")
+    object_count = queryset.count()
+    print(f"\nCompute detected_license_expression for {object_count:,} resources.")
 
     chunk_size = 2000
-    for index, resource in enumerate(resources.iterator(chunk_size=chunk_size)):
-        if not (index % chunk_size):
-            print(f"  {index:,} / {resource_count:,}")
+    iterator = queryset.iterator(chunk_size=chunk_size)
 
+    unsaved_objects = []
+    for index, resource in enumerate(iterator, start=1):
         combined_expression = str(combine_expressions(resource.license_expressions))
-        license_expression_spdx = build_spdx_license_expression(combined_expression)
-        resource.declared_license_expression = combined_expression
-        resource.declared_license_expression_spdx = license_expression_spdx
-        resource.save()
+        # TODO:
+        # gpl-2.0 OR broadcom-linking-unmodified OR proprietary-license
+        # build_spdx_license_expression("broadcom-linking-unmodified")
+        # AttributeError: 'LicenseSymbol' object has no attribute 'wrapped'
+        try:
+            license_expression_spdx = build_spdx_license_expression(combined_expression)
+        except AttributeError:
+            continue
+
+        resource.detected_license_expression = combined_expression
+        resource.detected_license_expression_spdx = license_expression_spdx
+        unsaved_objects.append(resource)
+
+        if not (index % chunk_size) and unsaved_objects:
+            print(f"  {index:,} / {object_count:,} computed")
+
+    print("Updating DB objects...")
+    CodebaseResource.objects.bulk_update(
+        objs=unsaved_objects,
+        fields=[
+            "detected_license_expression",
+            "detected_license_expression_spdx",
+        ],
+        batch_size=1000,
+    )
 
 
 def _convert_matches_to_detections(license_matches):
@@ -111,19 +145,31 @@ def _convert_matches_to_detections(license_matches):
 def compute_resource_license_detections(apps, schema_editor):
     """Compute CodebaseResource `license_detections` from old `licenses` field."""
     CodebaseResource = apps.get_model("scanpipe", "CodebaseResource")
-    resources = CodebaseResource.objects.filter(~Q(licenses=[])).only("licenses")
+    queryset = CodebaseResource.objects.filter(~Q(licenses=[])).only("licenses")
 
-    resource_count = resources.count()
-    print(f"Compute license_detections for {resource_count:,} resources.")
+    object_count = queryset.count()
+    print(f"\nCompute license_detections for {object_count:,} resources.")
 
     chunk_size = 2000
-    for index, resource in enumerate(resources.iterator(chunk_size=chunk_size)):
-        if not (index % chunk_size):
-            print(f"  {index:,} / {resource_count:,}")
+    iterator = queryset.iterator(chunk_size=chunk_size)
 
+    unsaved_objects = []
+    for index, resource in enumerate(iterator, start=1):
         detections = _convert_matches_to_detections(resource.licenses)
         resource.license_detections = detections
-        resource.save()
+        unsaved_objects.append(resource)
+
+        if not (index % chunk_size):
+            print(f"  {index:,} / {object_count:,} computed")
+
+    print("Updating DB objects...")
+    # Keeping the batch_size small as the `license_detections` content is often large,
+    # and it may raise `django.db.utils.OperationalError: out of memory`
+    CodebaseResource.objects.bulk_update(
+        objs=unsaved_objects,
+        fields=["license_detections"],
+        batch_size=50,
+    )
 
 
 class Migration(migrations.Migration):
