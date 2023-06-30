128df6f52becca1de812a8619d8fc7c4b5285e6e
Thomas Druez <tdruez@nexb.com>
2023-06-30 15:58:22 +0400

    Include the git describe tag when possible in version #500
    Signed-off-by: Thomas Druez <tdruez@nexb.com>

--- scancodeio/__init__.py
+++ scancodeio/__init__.py
@@ -21,13 +21,60 @@
 # Visit https://github.com/nexB/scancode.io for support and download.
 
 import os
+import subprocess
 import sys
 import warnings
 from pathlib import Path
 
-__version__ = "32.3.0"
+VERSION = "32.3.0"
 
-SCAN_NOTICE = Path(__file__).resolve().parent.joinpath("scan.NOTICE").read_text()
+PROJECT_DIR = Path(__file__).resolve().parent
+ROOT_DIR = PROJECT_DIR.parent
+SCAN_NOTICE = PROJECT_DIR.joinpath("scan.NOTICE").read_text()
+
+
+def get_version(version):
+    """Return the version including the git describe tag when available."""
+    # The codebase is a git clone
+    if git_describe := get_git_describe_from_command():
+        return git_describe
+
+    # The codebase is an extracted git archive
+    if git_describe := get_git_describe_from_version_file():
+        return git_describe
+
+    return version
+
+
+def get_git_describe_from_command():
+    """
+    Return the git describe tag from executing the ``git describe --tags`` command.
+    This will only provide a result when the codebase is a git clone.
+    """
+    git_describe = subprocess.run(
+        "git describe --tags",
+        capture_output=True,
+        shell=True,
+        text=True,
+    )
+    return git_describe.stdout.strip()
+
+
+def get_git_describe_from_version_file(version_file_location=ROOT_DIR / ".VERSION"):
+    """
+    Return the git describe tag from the ".VERSION" file.
+    This will only provide a result when the codebase is an extracted git archive
+    """
+    try:
+        version = version_file_location.read_text().strip()
+    except (FileNotFoundError, UnicodeDecodeError):
+        return
+
+    if version and version.startswith("v"):
+        return version
+
+
+__version__ = get_version(VERSION)
 
 
 # Turn off the warnings for the following modules.
