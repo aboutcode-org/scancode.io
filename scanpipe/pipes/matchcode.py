a581cf3aa2fa696138372ec1db931c1e6aa9009d
Jono Yang <jyang@nexb.com>
2023-08-02 15:54:40 -0700

    Generalize fingerprint_codebase_directories #781
        * This helps during the package indexing process on matchcode as well
    
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- scanpipe/pipes/matchcode.py
+++ scanpipe/pipes/matchcode.py
@@ -20,37 +20,56 @@
 # ScanCode.io is a free software code scanning tool from nexB Inc. and others.
 # Visit https://github.com/nexB/scancode.io for support and download.
 
-from matchcode_toolkit.fingerprinting import compute_directory_fingerprints
+from matchcode_toolkit.fingerprinting import compute_codebase_directory_fingerprints
+
+from scanpipe.pipes import codebase
 
 
-def fingerprint_codebase_directories(project, virtual_codebase):
-    """Compute directory fingerprints for matching purposes"""
-    # Compute directory fingerprints in memory
-    vc_to_directory = virtual_codebase.get_resource("to")
-    _ = compute_directory_fingerprints(vc_to_directory, virtual_codebase)
+def save_directory_fingerprints(project, virtual_codebase, to_codebase_only=False):
+    """
+    Save directory fingerprints from directory Resources in `virtual_codebase`
+    to the directory CodebaseResources from `project` that have the same path.
+
+    If `to_codebase_only` is True, then we are only saving the directory
+    fingerprints for directories from the to/ codebase of a d2d project.
+    """
 
     # Bulk update Directories with new fingerprints.
     # Code adapted from
     # scanpipe.migrations.0031_scancode_toolkit_v32_data_updates
-    queryset = project.codebaseresources.directories().to_codebase()
+    queryset = project.codebaseresources.directories()
+    if to_codebase_only:
+        queryset = queryset.to_codebase()
+
     object_count = queryset.count()
     print(f"\nUpdating directory fingerprints for {object_count:,} directories.")
     chunk_size = 2000
     iterator = queryset.iterator(chunk_size=chunk_size)
 
     unsaved_objects = []
-    for index, resource in enumerate(iterator, start=1):
-        vc_resource = virtual_codebase.get_resource(resource.path)
+    has_virtual_codebase_prefix = virtual_codebase.root.path == "virtual_codebase"
+    for index, directory in enumerate(iterator, start=1):
+        if has_virtual_codebase_prefix:
+            vc_path = f"virtual_codebase/{directory.path}"
+        else:
+            vc_path = directory.path
+
+        vc_directory = virtual_codebase.get_resource(vc_path)
+        if not vc_directory:
+            # Generally, `virtual_codebase` should contain the Resources and
+            # Directories that we care to create fingerprints for.
+            # If `directory` is not in `virtual_codebase`, we can skip it.
+            continue
 
         extra_data_keys = [
             "directory_content",
             "directory_structure",
         ]
         for key in extra_data_keys:
-            value = vc_resource.extra_data.get(key)
-            resource.extra_data[key] = value
+            value = vc_directory.extra_data.get(key, "")
+            directory.extra_data[key] = value
 
-        unsaved_objects.append(resource)
+        unsaved_objects.append(directory)
 
         if not (index % chunk_size) and unsaved_objects:
             print(f"  {index:,} / {object_count:,} directories processed")
@@ -61,3 +80,18 @@ def fingerprint_codebase_directories(project, virtual_codebase):
         fields=["extra_data"],
         batch_size=1000,
     )
+
+
+def fingerprint_codebase_directories(project, to_codebase_only=False):
+    """
+    Compute directory fingerprints for the directories of the to/ codebase from
+    `project`.
+
+    These directory fingerprints are used for matching purposes on matchcode.
+    """
+    resources = project.codebaseresources.all()
+    if to_codebase_only:
+        resources = resources.to_codebase()
+    virtual_codebase = codebase.get_basic_virtual_codebase(resources)
+    virtual_codebase = compute_codebase_directory_fingerprints(virtual_codebase)
+    save_directory_fingerprints(project, virtual_codebase, to_codebase_only=to_codebase_only)
