415aaad3eb57d3138ad86974578b0cfdc2099eb1
Thomas Druez <tdruez@nexb.com>
2023-09-01 16:57:11 +0200

    Add unit test for next run execution on success #901
    Signed-off-by: Thomas Druez <tdruez@nexb.com>

--- scanpipe/tests/test_tasks.py
+++ scanpipe/tests/test_tasks.py
@@ -43,3 +43,43 @@ class ScanPipeTasksTest(TestCase):
         self.assertEqual("", run.task_output)
         self.assertIsNotNone(run.task_start_date)
         self.assertIsNotNone(run.task_end_date)
+
+    @mock.patch("scanpipe.pipelines.Pipeline.execute")
+    def test_scanpipe_tasks_execute_pipeline_run_next_on_success(self, mock_execute):
+        project = Project.objects.create(name="my_project")
+        run = project.add_pipeline("do_nothing")
+        run2 = project.add_pipeline("do_nothing")
+
+        mock_execute.return_value = 0, ""
+        tasks.execute_pipeline_task(run.pk)
+        mock_execute.assert_called()
+        self.assertEqual(2, mock_execute.call_count)
+
+        run.refresh_from_db()
+        self.assertEqual(0, run.task_exitcode)
+        self.assertEqual("", run.task_output)
+        run2.refresh_from_db()
+        self.assertEqual(0, run2.task_exitcode)
+        self.assertEqual("", run2.task_output)
+
+    @mock.patch("scanpipe.pipelines.Pipeline.execute")
+    def test_scanpipe_tasks_execute_pipeline_no_run_next_on_failure(self, mock_execute):
+        project = Project.objects.create(name="my_project")
+        run = project.add_pipeline("do_nothing")
+        run2 = project.add_pipeline("do_nothing")
+
+        mock_execute.return_value = 1, "error"
+        tasks.execute_pipeline_task(run.pk)
+        mock_execute.assert_called_once()
+
+        run.refresh_from_db()
+        self.assertEqual(1, run.task_exitcode)
+        self.assertEqual("error", run.task_output)
+        self.assertIsNotNone(run.task_start_date)
+        self.assertIsNotNone(run.task_end_date)
+
+        run2.refresh_from_db()
+        self.assertIsNone(run2.task_exitcode)
+        self.assertEqual("", run2.task_output)
+        self.assertIsNone(run2.task_start_date)
+        self.assertIsNone(run2.task_end_date)
