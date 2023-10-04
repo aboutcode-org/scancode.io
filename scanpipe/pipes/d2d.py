f6812055d2e1f0d2beb39215dfa7e6d113ec95c5
Keshav Priyadarshi <git@keshav.space>
2023-10-04 09:51:23 +0530

    Limit npm package lookup to resource without status (#952)
    * Limit npm package lookup to resource without status
    
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>
    
    * Address review
    
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>
    
    ---------
    
    Signed-off-by: Keshav Priyadarshi <git@keshav.space>

--- scanpipe/pipes/d2d.py
+++ scanpipe/pipes/d2d.py
@@ -1136,7 +1136,7 @@ def map_thirdparty_npm_packages(project, logger=None):
         .exclude(path__regex=r"^.*\/node_modules\/.*\/node_modules\/.*$")
     )
 
-    to_resources = project_files.to_codebase()
+    to_resources = project_files.to_codebase().no_status()
     resource_count = to_package_json.count()
 
     if logger:
@@ -1164,9 +1164,9 @@ def _map_thirdparty_npm_packages(package_json, to_resources, project):
 
     package_resources = to_resources.filter(path__startswith=path_parent)
 
-    if not all(
-        [package, package.type, package.name, package.version, package_resources]
-    ):
+    purl_in_package = all([package, package.type, package.name, package.version])
+
+    if not package_resources or not purl_in_package:
         return 0
 
     package_data = package.to_dict()
