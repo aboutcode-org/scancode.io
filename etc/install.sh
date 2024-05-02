ddb82a8da4786021d8e477594432ec8736a24def
tdruez <tdruez@nexb.com>
2024-05-02 14:34:55 +0400

    Refine the implementation of the installation script
    Signed-off-by: tdruez <tdruez@nexb.com>

--- etc/install.sh
+++ etc/install.sh
@@ -11,6 +11,7 @@
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/nexB/scancode.io/install-sh/etc/install.sh)"
 
 INSTALL_LOCATION=${HOME}/.scancodeio
+BIN_LOCATION=${INSTALL_LOCATION}/bin
 ENV_FILE=${INSTALL_LOCATION}/.env
 DOCKER_COMPOSE_URL="https://raw.githubusercontent.com/nexB/scancode.io/install-sh/docker-compose.latest.yml"
 DOCKER_ENV_URL="https://raw.githubusercontent.com/nexB/scancode.io/install-sh/docker.env"
@@ -19,9 +20,11 @@ DOCKER_ENV_FILE=${INSTALL_LOCATION}/docker.env
 DOCKER_COMPOSE="docker compose -f ${DOCKER_COMPOSE_FILE}"
 DOCKER_VOLUMES="--volume ${INSTALL_LOCATION}:/var/scancodeio/workspace/"
 SCANPIPE_CMD="${DOCKER_COMPOSE} run ${DOCKER_VOLUMES} --rm web scanpipe"
+SCANPIPE_BIN_FILE=${INSTALL_LOCATION}/bin/scanpipe
 
-# Create the ~/.scancodeio directory in home
+# Create the ~/.scancodeio and bin/ directories in $HOME
 mkdir -p ${INSTALL_LOCATION}
+mkdir -p ${BIN_LOCATION}
 
 # Generate the environment file
 if [ ! -f "${ENV_FILE}" ]; then
@@ -30,17 +33,21 @@ if [ ! -f "${ENV_FILE}" ]; then
     echo SECRET_KEY=\"TODO\" > ${ENV_FILE}
 fi
 
-# Fetch the docker-compose.yml and docker.env files
-curl --output ${DOCKER_COMPOSE_FILE} ${DOCKER_COMPOSE_URL}
-curl --output ${DOCKER_ENV_FILE} ${DOCKER_ENV_URL}
+echo "-> Fetch the docker-compose.yml and docker.env files"
+curl --silent --output ${DOCKER_COMPOSE_FILE} ${DOCKER_COMPOSE_URL}
+curl --silent --output ${DOCKER_ENV_FILE} ${DOCKER_ENV_URL}
 
-# Run a test command
-${SCANPIPE_CMD} create-project test_project
+if [ ! -f "${SCANPIPE_BIN_FILE}" ]; then
+    echo "-> Create the scanpipe executable in local bin/"
+    touch ${SCANPIPE_BIN_FILE}
+    chmod a+x ${SCANPIPE_BIN_FILE}
+    echo "#!/bin/bash" > ${SCANPIPE_BIN_FILE}
+    echo "${SCANPIPE_CMD} \$@" >> ${SCANPIPE_BIN_FILE}
+fi
 
-#${SCANPIPE_CMD} create-project ackage-url \
-#    --input-url https://github.com/package-url/packageurl-js/archive/refs/tags/v1.2.1.zip \
-#    --pipeline scan_codebase \
-#    --execute
-#${SCANPIPE_CMD} shell
+echo "Installation completed."
+echo "You can now run ScanPipe commands using: ${SCANPIPE_BIN_FILE} [COMMAND]"
 
-echo "Installation completed!"
+# TODO (requires sudo): sudo ln -sf ${SCANPIPE_BIN_FILE} /usr/local/bin/scanpipe
+# Alternatively, set the $BIN_LOCATION in $PATH with:
+# echo "export PATH=\"${BIN_LOCATION}:\$PATH\""
