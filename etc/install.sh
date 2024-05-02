372ac0224343b6fb79452a9b82235863eeeac4aa
tdruez <tdruez@nexb.com>
2024-05-02 15:15:31 +0400

    Refine the implementation of the installation script
    Signed-off-by: tdruez <tdruez@nexb.com>

--- etc/install.sh
+++ etc/install.sh
@@ -19,35 +19,51 @@ DOCKER_COMPOSE_FILE=${INSTALL_LOCATION}/docker-compose.yml
 DOCKER_ENV_FILE=${INSTALL_LOCATION}/docker.env
 DOCKER_COMPOSE="docker compose -f ${DOCKER_COMPOSE_FILE}"
 DOCKER_VOLUMES="--volume ${INSTALL_LOCATION}:/var/scancodeio/workspace/"
-SCANPIPE_CMD="${DOCKER_COMPOSE} run ${DOCKER_VOLUMES} --rm web scanpipe"
+#SCANPIPE_CMD="${DOCKER_COMPOSE} run ${DOCKER_VOLUMES} --rm web scanpipe"
+# TODO: Mounting volume raise Permission denied on Ubuntu
+SCANPIPE_CMD="${DOCKER_COMPOSE} run --rm web scanpipe"
 SCANPIPE_BIN_FILE=${INSTALL_LOCATION}/bin/scanpipe
 
-# Create the ~/.scancodeio and bin/ directories in $HOME
+success() {
+  printf "\033[1;32m%s\033[0m\n" "$1"
+}
+
+info() {
+  printf "\033[1m-> %s\033[0m\n" "$1"
+}
+
+info "Install ScanCode.io at ${INSTALL_LOCATION}"
 mkdir -p ${INSTALL_LOCATION}
 mkdir -p ${BIN_LOCATION}
 
 # Generate the environment file
 if [ ! -f "${ENV_FILE}" ]; then
-    echo "-> Create the .env file and generate a secret key"
+    info "Create the .env file and generate a secret key"
     touch ${ENV_FILE}
     echo SECRET_KEY=\"TODO\" > ${ENV_FILE}
 fi
 
-echo "-> Fetch the docker-compose.yml and docker.env files"
+info "Fetch the docker-compose.yml and docker.env files"
 curl --silent --output ${DOCKER_COMPOSE_FILE} ${DOCKER_COMPOSE_URL}
 curl --silent --output ${DOCKER_ENV_FILE} ${DOCKER_ENV_URL}
 
 if [ ! -f "${SCANPIPE_BIN_FILE}" ]; then
-    echo "-> Create the scanpipe executable in local bin/"
+    echo "Create the scanpipe executable in local bin/"
     touch ${SCANPIPE_BIN_FILE}
     chmod a+x ${SCANPIPE_BIN_FILE}
     echo "#!/bin/bash" > ${SCANPIPE_BIN_FILE}
     echo "${SCANPIPE_CMD} \$@" >> ${SCANPIPE_BIN_FILE}
 fi
 
-echo "Installation completed."
-echo "You can now run ScanPipe commands using: ${SCANPIPE_BIN_FILE} [COMMAND]"
+info "Prepare the database"
+${SCANPIPE_CMD} migrate
+
+success "Installation completed."
+success "You can run the scanpipe commands using: ${SCANPIPE_BIN_FILE} [COMMAND]"
 
 # TODO (requires sudo): sudo ln -sf ${SCANPIPE_BIN_FILE} /usr/local/bin/scanpipe
 # Alternatively, set the $BIN_LOCATION in $PATH with:
 # echo "export PATH=\"${BIN_LOCATION}:\$PATH\""
+
+# To stop all related services:
+# docker compose -f ${HOME}/.scancodeio/docker-compose.yml stop
