b4e1640a7af88560ffbdeca443623adc1bdb6f21
tdruez <tdruez@nexb.com>
2024-11-06 12:25:26 +0400

    Debug failing tests on CI #386
    Signed-off-by: tdruez <tdruez@nexb.com>

--- scanpipe/tests/test_policies.py
+++ scanpipe/tests/test_policies.py
@@ -107,6 +107,7 @@ class ScanPipePoliciesTest(TestCase):
 
         self.assertEqual(0, exitcode, msg=out)
         resource_qs = project1.codebaseresources
+        print(resource_qs)  # DEBUG statement
         self.assertEqual(6, resource_qs.count())
 
         resource = resource_qs.get(name="apache-2.0.LICENSE")
