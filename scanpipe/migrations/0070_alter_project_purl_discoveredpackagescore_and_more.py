8679ddc5c8bc8e0fe4a2e8ce897b2b80e2f160ab
404-geek <pranayd61@gmail.com>
2025-03-01 22:47:10 -0500

    change migration file and update `ScoreCode` version
    Signed-off-by: 404-geek <pranayd61@gmail.com>

--- scanpipe/migrations/0070_alter_project_purl_discoveredpackagescore_and_more.py
+++ scanpipe/migrations/0070_alter_project_purl_discoveredpackagescore_and_more.py
@@ -12,11 +12,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='project',
-            name='purl',
-            field=models.CharField(blank=True, help_text="Package URL (PURL) for the project, required for pushing the project's scan result to FederatedCode. For example, if the input is an input URL like https://registry.npmjs.org/lodash/-/lodash-4.17.21.tgz, the corresponding PURL would be pkg:npm/lodash@4.17.21.", max_length=2048),
-        ),
         migrations.CreateModel(
             name='DiscoveredPackageScore',
             fields=[
