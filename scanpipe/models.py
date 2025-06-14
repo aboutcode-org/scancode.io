912fc7f7ed17930fe78bc4b8544b16a8c1f79c79
Aayush Kumar <aayush214.kumar@gmail.com>
2025-06-14 23:59:01 +0530

    add save() method to CodebaseResource to ensure `parent_directory_path` is always set
    Signed-off-by: Aayush Kumar <aayush214.kumar@gmail.com>

--- scanpipe/models.py
+++ scanpipe/models.py
@@ -2796,6 +2796,11 @@ class CodebaseResource(
     def __str__(self):
         return self.path
 
+    def save(self, *args, **kwargs):
+        if self.path and not self.parent_directory_path:
+            self.parent_directory_path = parent_directory(str(self.path), with_trail=False)
+        super().save(*args, **kwargs)
+
     @property
     def location_path(self):
         """Return the location of the resource as a Path instance."""
