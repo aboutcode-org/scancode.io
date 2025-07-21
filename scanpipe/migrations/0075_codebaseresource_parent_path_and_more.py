154d1a637244f43bf93d269b322bbb3be34a8ccb
Aayush Kumar <aayush214.kumar@gmail.com>
2025-07-22 01:22:27 +0530

    fix code format
    Signed-off-by: Aayush Kumar <aayush214.kumar@gmail.com>

--- scanpipe/migrations/0075_codebaseresource_parent_path_and_more.py
+++ scanpipe/migrations/0075_codebaseresource_parent_path_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('scanpipe', '0073_add_sha1_git_checksum'),
+        ('scanpipe', '0074_discovered_license_models'),
     ]
 
     operations = [
