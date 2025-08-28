5bf571d1f1a8cddcc286934b3a781ae684d2b81b
Jono Yang <jyang@nexb.com>
2025-08-28 16:01:57 -0700

    Separate commiting and pushing functionality in federatedcode pipes
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- scanpipe/pipes/federatedcode.py
+++ scanpipe/pipes/federatedcode.py
@@ -161,6 +161,19 @@ def add_scan_result(project, repo, package_scan_file, logger=None):
     return relative_scan_file_path
 
 
+def commit_changes(repo, files_to_commit, commit_message):
+    """Commit changes to remote repository."""
+    repo.index.add(files_to_commit)
+    repo.index.commit(textwrap.dedent(commit_message))
+
+
+def push_changes(repo, remote_name="origin", branch_name=""):
+    """Push changes to remote repository."""
+    if not branch_name:
+        branch_name = repo.active_branch.name
+    repo.git.push(remote_name, branch_name, "--no-verify")
+
+
 def commit_and_push_changes(
     repo, file_to_commit, purl, remote_name="origin", logger=None
 ):
@@ -177,12 +190,16 @@ def commit_and_push_changes(
 
     Signed-off-by: {author_name} <{author_email}>
     """
-
-    default_branch = repo.active_branch.name
-
-    repo.index.add([file_to_commit])
-    repo.index.commit(textwrap.dedent(commit_message))
-    repo.git.push(remote_name, default_branch, "--no-verify")
+    files_to_commit = [file_to_commit]
+    commit_changes(
+        repo=repo,
+        files_to_commit=files_to_commit,
+        commit_message=commit_message
+    )
+    push_changes(
+        repo=repo,
+        remote_name=remote_name,
+    )
 
 
 def delete_local_clone(repo):
