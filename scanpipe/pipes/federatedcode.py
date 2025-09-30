b92b4f57802dd85eafba57b896e1c6d5dff2b6a3
ziad hany <ziadhany2016@gmail.com>
2025-09-30 13:10:54 +0300

    Rename full_history to shallow_clone for clarity
    Signed-off-by: ziad hany <ziadhany2016@gmail.com>

--- scanpipe/pipes/federatedcode.py
+++ scanpipe/pipes/federatedcode.py
@@ -145,7 +145,7 @@ def check_federatedcode_configured_and_available(logger=None):
         logger("Federatedcode repositories are configured and available.")
 
 
-def clone_repository(repo_url, full_history=False, logger=None):
+def clone_repository(repo_url, shallow_clone=True, logger=None):
     """Clone repository to local_path."""
     local_dir = tempfile.mkdtemp()
 
@@ -154,10 +154,10 @@ def clone_repository(repo_url, full_history=False, logger=None):
         f"https://{settings.FEDERATEDCODE_GIT_SERVICE_TOKEN}@",
     )
 
-    if full_history:
-        repo = Repo.clone_from(url=authenticated_repo_url, to_path=local_dir)
-    else:
+    if shallow_clone:
         repo = Repo.clone_from(url=authenticated_repo_url, to_path=local_dir, depth=1)
+    else:
+        repo = Repo.clone_from(url=authenticated_repo_url, to_path=local_dir)
 
     repo.config_writer(config_level="repository").set_value(
         "user", "name", settings.FEDERATEDCODE_GIT_SERVICE_NAME
