e050dded6fa9a0cebb7a62369a1593d9e001e67c
tdruez <tdruez@aboutcode.org>
2025-12-29 09:25:35 +0400

    Add project vulnerability list view
    Signed-off-by: tdruez <tdruez@aboutcode.org>

--- scanpipe/tests/test_views.py
+++ scanpipe/tests/test_views.py
@@ -821,7 +821,7 @@ class ScanPipeViewsTest(TestCase):
         with self.assertNumQueries(7):
             self.client.get(url)
 
-        with self.assertNumQueries(13):
+        with self.assertNumQueries(15):
             self.client.get(self.project1.get_absolute_url())
 
     @mock.patch("scanpipe.models.Run.execute_task_async")
