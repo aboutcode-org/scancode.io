e540c9a4b83941a0f3c4412e61cebb4cdaa26ce2
tdruez <tdruez@aboutcode.org>
2026-03-16 19:16:01 +0400

    remove the need for a base_htmx.html template
    Signed-off-by: tdruez <tdruez@aboutcode.org>

--- scanpipe/views.py
+++ scanpipe/views.py
@@ -2096,6 +2096,11 @@ class CodebaseResourceDetailsView(
     def get_queryset(self):
         return super().get_queryset().select_related("project")
 
+    def get_template_names(self):
+        if self.request.htmx:
+            return ["scanpipe/partials/resource_detail_content.html"]
+        return ["scanpipe/resource_detail.html"]
+
     @staticmethod
     def get_annotations(entries, value_key):
         annotations = []
