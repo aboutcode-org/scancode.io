d99c2a3ce52d10c6dcebd09c4258411b646a2144
ziad hany <ziadhany2016@gmail.com>
2026-07-27 22:08:22 +0300

    Add initial support for the npm-health ScanGrimoireLab pipeline
    Signed-off-by: ziad hany <ziadhany2016@gmail.com>

---
+++ scanpipe/tests/pipes/test_scan_repo_grimoirelab.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+class ScanGrimoirelabTest(TestCase):
+    def test_collect_grimoire_metric(self):
+        raise NotImplementedError
+
+    def test_compute_and_store_metric_score(self):
+        raise NotImplementedError
