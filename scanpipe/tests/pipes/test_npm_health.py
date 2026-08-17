4ddcaf08e6cba4780ccea508fa6c11812d0e5c2b
Luca Magrini <89993099+Berserk-hub150@users.noreply.github.com>
2026-08-17 21:35:00 +0200

    45/70 npm-health tests: cover valid npm PURLs
    Signed-off-by: Luca Magrini <89993099+Berserk-hub150@users.noreply.github.com>

---
+++ scanpipe/tests/pipes/test_npm_health.py
@@ -0,0 +1,34 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# http://nexb.com and https://github.com/aboutcode-org/scancode.io
+# The ScanCode.io software is licensed under the Apache License version 2.0.
+# Data generated with ScanCode.io is provided as-is without warranties.
+# ScanCode is a trademark of nexB Inc.
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+"""Tests for npm-health helper utilities."""
+
+from datetime import UTC
+from datetime import datetime
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from unittest import mock
+
+from django.test import SimpleTestCase
+
+from scanpipe.pipes import npm_health
+
+
+class NpmHealthPipesTest(SimpleTestCase):
+
+    def test_validate_npm_package_url(self):
+        package = npm_health.validate_npm_package_url("pkg:npm/lodash@4.17.21")
+        self.assertEqual("npm", package.type)
+        self.assertEqual("lodash", package.name)
+        self.assertEqual("4.17.21", package.version)
