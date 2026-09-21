2eb1b92af836dcf8b0d2068bcfc52ea5491b96c8
Chin Yeung <tli@nexb.com>
2026-09-21 14:51:13 +0800

    fix: ensure the status was not updated after scanned #2228 (#2229)
    Signed-off-by: Chin Yeung Li <tli@nexb.com>

--- scanpipe/pipes/d2d.py
+++ scanpipe/pipes/d2d.py
@@ -1677,9 +1677,13 @@ def scan_ignored_to_files(project, logger=None):
         .to_codebase()
         .filter(status=flag.IGNORED_FROM_CONFIG)
     )
-    scancode.scan_for_files(project, scan_files, progress_logger=logger)
+    scan_file_ids = list(scan_files.values_list("id", flat=True))
 
-    project.codebaseresources.files().to_codebase().filter(status=flag.SCANNED).update(
+    if not scan_file_ids:
+        return
+
+    scancode.scan_for_files(project, scan_files, progress_logger=logger)
+    project.codebaseresources.filter(id__in=scan_file_ids, status=flag.SCANNED).update(
         status=flag.IGNORED_FROM_CONFIG
     )
 
@@ -1694,9 +1698,13 @@ def scan_unmapped_to_files(project, logger=None):
         .to_codebase()
         .filter(status=flag.REQUIRES_REVIEW)
     )
-    scancode.scan_for_files(project, scan_files, progress_logger=logger)
+    scan_file_ids = list(scan_files.values_list("id", flat=True))
 
-    project.codebaseresources.files().to_codebase().filter(status=flag.SCANNED).update(
+    if not scan_file_ids:
+        return
+
+    scancode.scan_for_files(project, scan_files, progress_logger=logger)
+    project.codebaseresources.filter(id__in=scan_file_ids, status=flag.SCANNED).update(
         status=flag.REQUIRES_REVIEW
     )
 
